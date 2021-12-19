206200f277fadb001c5f462c85797e42586a4ae4
qingshui-hui <stakanari987@gmail.com>
2021-12-19 18:55:58 +0900

    socket使用条件のミス修正

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -1,20 +1,10 @@
-
-function parseBool(val) {
-  if (!val) {
-    return false
-  }
-  if (val === 'false' || val === '0') {
-    return false
-  }
-  return true
-}
-
 import { io } from 'socket.io-client'
 
 export default {
   install: (app) => {
     let socket
-    if (!parseBool(process.env.VUE_APP_WS_ENABLED)) {
+    // 環境変数がfalseか0ならsocketを使わない。
+    if (['false', '0'].includes(process.env.VUE_APP_WS_ENABLED)) {
       socket = null
     } else if (process.env.VUE_APP_WS_HOST) {
       socket = io(process.env.VUE_APP_WS_HOST)
