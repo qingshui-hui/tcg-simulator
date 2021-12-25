6af1a3db8fa965c8dfa9715e0fc20a11823a455f
qingshui-hui <stakanari987@gmail.com>
2021-12-25 12:59:20 +0900

    部屋選択のページを修正

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,22 +2,36 @@
   <div>
     <div id="app" style="padding: 20px">
       <p>部屋を選択してください</p>
-      <div v-for="room in rooms" :key="room.id" style="margin-top: 10px;">
-        <router-link
-          class="send"
-          :to="{
-            path: '/room',
-            query: { roomId: room.id, player: 'a' },
-          }"
-        >room{{ room.id }} プレイヤーA</router-link>
-        <router-link
-          class="send"
-          :to="{
-            path: '/room',
-            query: { roomId: room.id, player: 'b' },
-          }"
-        >room{{ room.id }} プレイヤーB</router-link>
-      </div>
+      <table class="roomTable" style="margin-top: 20px">
+        <thead>
+          <th></th>
+          <th>プレイヤーA</th>
+          <th>プレイヤーB</th>
+        </thead>
+        <tr v-for="room in rooms" :key="room.id">
+          <td>部屋{{ room.id }}</td>
+          <td>
+            <router-link
+              :to="{
+                path: '/room',
+                query: { roomId: room.id, player: 'a' },
+              }"
+            >
+              <o-button variant="info" size="small"> {{ room.id }}-A</o-button>
+            </router-link>
+          </td>
+          <td>
+            <router-link
+              :to="{
+                path: '/room',
+                query: { roomId: room.id, player: 'b' },
+              }"
+            >
+              <o-button variant="info" size="small"> {{ room.id }}-B</o-button>
+            </router-link>
+          </td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
@@ -26,18 +40,25 @@
 export default {
   data() {
     return {
-      rooms: [
-        { id: '1' },
-        { id: '2' },
-        { id: '3' },
-      ]
-    }
-  }
-}
+      rooms: [{ id: "1" }, { id: "2" }, { id: "3" }],
+    };
+  },
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .send {
   margin-left: 10px;
+}
+.roomTable {
+  border-collapse: collapse;
+  th,
+  td {
+    border: 1px solid darkgray;
+    padding: 5px 10px;
+  }
+}
+a {
+  text-decoration: none;
 }
 </style>
