61a6224e47d3dca678a970a22f4a844ba726d139
qingshui-hui <stakanari987@gmail.com>
2022-02-24 00:31:22 +0900

    yarn testが通るように修正

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,32 +1,38 @@
-import { config, RouterLinkStub } from '@vue/test-utils'
+import { config, RouterLinkStub } from "@vue/test-utils";
 
 // @ts-nocheck
-import { createApp } from 'vue'
-import App from '@/app'
+import { createApp } from "vue";
+import App from "@/app";
 
-const vueApp = createApp(App)
+const vueApp = createApp(App);
 
 // vue router
-import router from '@/router'
-vueApp.use(router)
+import router from "@/router";
+vueApp.use(router);
 
 // vuex
-import store from '@/store'
-vueApp.use(store)
+import store from "@/store";
+vueApp.use(store);
 
 //
 // useConfig
-import useConfig from '@/plugins/useConfig'
-vueApp.use(useConfig)
+import useConfig from "@/plugins/useConfig";
+vueApp.use(useConfig);
 
 //
 // oruga ui
-import { useOruga } from '@/plugins/oruga'
-useOruga(vueApp)
+import { useOruga } from "@/plugins/oruga";
+useOruga(vueApp);
 
-import ClientOnlyStub from './stub/ClientOnlyStub.vue'
+import ClientOnlyStub from "./stub/ClientOnlyStub.vue";
 // Mock Router components
-config.global.stubs['router-link'] = RouterLinkStub
-config.global.stubs['client-noly'] = ClientOnlyStub
-config.global.stubs['dropdown'] = ClientOnlyStub
-config.global.stubs['o-icon'] = ClientOnlyStub
+config.global.stubs["router-link"] = RouterLinkStub;
+config.global.stubs["client-noly"] = ClientOnlyStub;
+config.global.stubs["dropdown"] = ClientOnlyStub;
+config.global.stubs["o-icon"] = ClientOnlyStub;
+
+// raw-loader
+// https://stackoverflow.com/questions/55118947/how-to-test-raw-loaded-data-with-react
+jest.mock("raw-loader!../assets/markdown/about.md", () => "", {
+  virtual: true,
+});
